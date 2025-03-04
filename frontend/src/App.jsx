cdbd7a5e05895fd7ae9543e9af9f86b608cbcdbc
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-04 22:48:03 +0800

    Added Routes and Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 
 
 const App = () => {
   return (
     <div>
-      
-    
+      <Navbar/>  
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
+      </Routes>
     </div>
     
-  )
-}
+  );
+};
 
 export default App;
