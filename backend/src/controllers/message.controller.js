ee599bfbffaac2bee797c7b15f8813a26ba25253
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-04 22:27:46 +0800

    Finished sendMessage and getMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 export const getUsersForSiderbar = async (req, res) => {
@@ -37,6 +38,35 @@ export const getMessages = async (req, res) => {
         
     } catch (error) {
         console.error("Error in getting messages: ", error.message);
+        res.status(500).json({message: 'Server error'});
+    }
+}
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+        
+        let imageUrl;
+
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;  
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+        
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error("Error in sending messages: ", error.message);
         res.status(500).json({message: 'Server error'});
     }
 }
