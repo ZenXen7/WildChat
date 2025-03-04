1c5b64c6db9c7264d471e58b29b3835069df8fd9
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-04 10:21:13 +0800

    Finished Signup Controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,10 +1,14 @@
-import User from "../models/user.model";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
     
     try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({ message: "Please fill all required fields." });
+        }
         if(password.length < 6){
             return res.status(400).json({ message: "Password must be at least 6 characters long." });
         }
@@ -23,13 +27,23 @@ export const signup = async (req, res) => {
              password: hashedPassword});
         
         if(newUser){
-            
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+
         }else{
             res.status(400).json({message: 'Invalid user data'});
         }
 
     } catch (error) {
-        
+        console.log("Error in signup", error.message);
+        res.status(500).json({message: 'Server error'});
     }
 }
 
