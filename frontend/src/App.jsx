12d1dd34a06306389e57412a33e438d35be3165a
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-05 10:55:44 +0800

    Fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,28 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
-
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import {Loader} from "lucide-react"
 
 const App = () => {
+ const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
  
+  
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({authUser});
+
+  if(isCheckingAuth && !authUser) return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
+
   return (
     <div>
       <Navbar/>  
