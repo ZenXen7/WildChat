518e52e7a53daa132aa2b1c8a6b0baa87115dea5
ZenXen7 <karlchristian.ajero@cit.edu>
2025-03-08 00:52:44 +0800

    Message Skeleton added and loading messages

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore"
-
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 
 
 const ChatContainer = () => {
@@ -10,13 +12,22 @@ const ChatContainer = () => {
     getMessages(selectedUser._id);
   }, [selectedUser._id, getMessages]);
 
-  if(isMessagesLoading) return <div>Loading...</div>
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
 
   
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader/>
-
+     
       <MessageInput/>      
     </div>
   )
